b41772e80d76114c5bb75807a642b18e9391d7b4
Hutson Betts <hutson@hyper-expanse.net>
2016-01-16 10:36:11 -0600

    refactor(gulp): Cleanup gulp configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var paths = {
     'gulpfile.js',
     'generators/**/*.js',
     '!generators/**/*.spec.js',
-    '!generators/**/templates/*.*',
+    '!generators/**/templates/**/*.*',
   ],
 
   documentationFiles: [
     'README.md',
     'generators/**/*.js',
     '!generators/**/*.spec.js',
-    '!generators/**/templates/*.*',
+    '!generators/**/templates/**/*.*',
   ],
 
   sourceFiles: [
@@ -29,26 +29,36 @@ var paths = {
 
   testFiles: [
     'generators/**/*.spec.js',
+    '!generators/**/templates/**/*.*',
   ],
 };
 
 /*
- * Clean up build directories.
+ * Clean up coverage directory.
  */
-gulp.task('clean', function() {
+gulp.task('clean:coverage', function() {
   var del = require('del');
 
   return del([
     'coverage/',
+  ]);
+});
+
+/*
+ * Clean up docs directory.
+ */
+gulp.task('clean:docs', function() {
+  var del = require('del');
+
+  return del([
     'docs/',
   ]);
 });
 
 /*
- * Generator a Cobertura coverage report.
- * TODO
+ * Execute unit and integration tests and generate a coverage reports.
  */
-gulp.task('test', ['clean'], function(done) {
+gulp.task('test', ['clean:coverage'], function(done) {
   var istanbul = require('gulp-istanbul');
   var mocha    = require('gulp-mocha');
 
@@ -63,7 +73,8 @@ gulp.task('test', ['clean'], function(done) {
         .pipe(istanbul.writeReports({
           reporters: ['text', 'lcov'],
         }))
-        .on('end', done);
+        .on('end', done)
+      ;
     });
 });
 
@@ -82,13 +93,13 @@ gulp.task('jscs', function() {
 
 /*
  * Generate source documentation.
- * TODO
  */
-gulp.task('docs', ['clean'], function(done) {
+gulp.task('docs', ['clean:docs'], function(done) {
   var jsdoc = require('gulp-jsdoc3');
 
+  console.log('directory', process.cwd());
   gulp.src(paths.documentationFiles, { read: false })
-    .pipe(jsdoc(JSON.parse(require('fs').readFileSync('./.jsdocrc')), done))
+    .pipe(jsdoc(JSON.parse(require('fs').readFileSync('./.jsdocrc'))), done)
   ;
 });
 
@@ -96,7 +107,7 @@ gulp.task('docs', ['clean'], function(done) {
  * Run source files through JSHint lint checks.
  */
 gulp.task('jshint', function() {
-  var jshint  = require('gulp-jshint');
+  var jshint = require('gulp-jshint');
 
   return gulp.src(paths.lintFiles)
     .pipe(jshint())
@@ -108,19 +119,14 @@ gulp.task('jshint', function() {
 /*
  * Watch for file changes to either source, or test, files, and execute the appropriate task(s) associated with the
  * changed file(s).
- * TODO
  */
-gulp.task('serve', ['default'], function() {
-  var watch = require('gulp-watch');
-
-  watch(paths.lintFiles, function() {
-    gulp.start('jshint');
-    gulp.start('jscs');
-  });
-
-  watch(paths.templateFiles.concat(paths.sourceFiles).concat(paths.testFiles), function() {
-    gulp.start('test:unit');
-  });
+gulp.task('watch', function() {
+  gulp.watch(paths.lintFiles, ['jshint', 'jscs']);
+  gulp.watch(
+    paths.templateFiles.concat(paths.sourceFiles).concat(paths.testFiles),
+    ['test']
+  );
+  gulp.watch(paths.documentationFiles, ['docs']);
 });
 
 gulp.task('default', ['jscs', 'jshint', 'test', 'docs']);
